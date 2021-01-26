2e205659e51a18b9db925f3213af554924d6afed
winson-cell <winsheng2000@gmail.com>
2021-01-26 18:54:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -14,21 +14,37 @@ npm install node-fetch
 //--------------------------------Account requirement----------------------------------------------------//
 
 //--------------------------------User Information Bar(Required to be filled)----------------------------------------------------//
-let userPrivateKey = ''//Private key in String
+let userPrivateKey = 'Enter Your Private Key'//Private key in String
 let userAction = 'claimdrop' // what action does the user acts 
 let userReferAccount = 'atomicdropsx' // Account that carries out the contract
-<<<<<<< Updated upstream
 let userClaimAccount = 'Enter Your Account name'// 'Enter the account name that received drop'  User Claim account in String
 let userClaimDropId = 1735//Claim id in number  link(https://wax.atomichub.io/drops/746)
 let userClaimAmount = 1//Claim amount in number
 let userIntendedDelphiMedian = 0 //Intended delphi median can be check at link(https://wax.bloks.io/account/delphioracle?loadContract=true&tab=Tables&table=datapoints&account=delphioracle&scope=waxpusd&limit=10) 
 let userCountry = "MY"//Country
 let userReferrer = 'atomichub' //Refer to atomuvhub
-=======
-let userClaimAccount = 'testingmoew2'// 'Enter the account name that received drop'  User Claim account in String
-let userClaimDropId = 1735 //Claim id in number  link(https://wax.atomichub.io/drops/746)
-let userClaimAmount = 1//Claim amount in number
+//--------------------------------User Information Bar----------------------------------------------------//
 
+//--------------------------------Timer----------------------------------------------------//
+
+while (true){
+    var time_now = new Date();
+    // new Date(year, month, day, hours, minutes, seconds, milliseconds)
+    // 0 is january
+    // Date(Year,month 0 = Jan,day,hour,minute,sec)
+    var buy_time = new Date(2021,0,26,9,35,0,0);
+    console.log(time_now)
+    if (time_now.getTime() == buy_time.getTime()){
+      console.log(buy_time);
+      console.log("yes")
+      break
+    }
+  }
+//--------------------------------Timer----------------------------------------------------//
+
+//--------------------------------Main----------------------------------------------------//
+
+//const { time } = require('console');
 const { Api, JsonRpc } = require('eosjs');
 const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig');  // development only
 const fetch = require('node-fetch'); //node only
@@ -38,36 +54,6 @@ const privateKeys = [userPrivateKey];
 const signatureProvider = new JsSignatureProvider(privateKeys);
 const rpc = new JsonRpc('https://chain.wax.io', { fetch }); //required to read blockchain state
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() }); //required to submit transactions
->>>>>>> Stashed changes
-//--------------------------------User Information Bar----------------------------------------------------//
-
-//--------------------------------Timer----------------------------------------------------//
-
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
-while (true){
-    var time_now = new Date();
-    // new Date(year, month, day, hours, minutes, seconds, milliseconds)
-    // 0 is january
-<<<<<<< Updated upstream
-    // Date(Year,month 0 = Jan,day,hour,minute,sec)
-    var buy_time = new Date(2021,0,26,9,35,0,0);
-=======
-    var buy_time = new Date(2021,0,26,11,1,0,0);
->>>>>>> Stashed changes
-    console.log(time_now)
-    if (time_now.getTime() == buy_time.getTime()){
-      break
-    }
-  }
-<<<<<<< Updated upstream
-//--------------------------------Timer----------------------------------------------------//
-=======
->>>>>>> Stashed changes
-
-//--------------------------------Timer----------------------------------------------------//
 
 //--------------------------------Function----------------------------------------------------//
 api.transact({
@@ -83,28 +69,16 @@ api.transact({
         claimer: userClaimAccount,
         country: userCountry,
         drop_id: userClaimDropId,
-<<<<<<< Updated upstream
         intended_delphi_median:userIntendedDelphiMedian,
         referrer:userReferrer,
-=======
-        intended_delphi_median:0,
-        referrer:'atomichub',
-        country:'MY'
->>>>>>> Stashed changes
       },
     }]
   }, {
   blocksBehind: 3,
   expireSeconds: 30,
-  }).then((res, err) =>{
-  if (res){
-    console.log(res)
-    console.log(res["transaction_id"])
-  }
-  if (err){
-    console.log(err)
-    console.log("no")
-  }
+  }).then((err, res) =>{
+  if (err) console.log(err)
+  if (res) console.log(require)
 })
 //--------------------------------Function----------------------------------------------------//
 //--------------------------------Main----------------------------------------------------//
