a9525ddf87c58d0b727b34d6a1bb7a9e41d6c327
winson-cell <winsheng2000@gmail.com>
2021-01-31 21:36:36 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,14 +2,25 @@ const { Api, JsonRpc } = require('eosjs');
 const fetch = require('node-fetch'); //node only
 const rpc = new JsonRpc('https://chain.wax.io', { fetch });
 
-let a = rpc.get_table_rows({
+var a = 0;
+async function doWork(){
+    const data = await rpc.get_table_rows({
         json: true,               // Get the response as json
         code: 'delphioracle',      // Contract that we target
         scope: 'waxpusd',         // Account that owns the data
         table: 'datapoints',        // Table name
-        limit: 10,                // Maximum number of rows that we want to get
+        limit: 1,                // Maximum number of rows that we want to get
         reverse: false,           // Optional: Get reversed data
         show_payer: false          // Optional: Show ram payer
-})
+    })
+    return data['rows'][0]['median']
+}
 
-console.log(a)
+/*
+a = doWork()
+console.log(a)*/
+
+(async () => {
+    a = await doWork()
+    console.log(a)
+  })()
